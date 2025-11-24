4201d23e3f459ff5934308821e09df8ce27c7298
magda_m <79257135+mikemachacky@users.noreply.github.com>
2025-11-24 13:57:19 +0100

    feat: add save and open

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QMainWindow, QApplication, QTextEdit, QMenuBar
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTextEdit, QMenuBar, QMessageBox, QFileDialog
 
 
 class MainWindow(QMainWindow):
@@ -9,6 +9,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.menu_bar = None
         self.text_edit = None
+        self.current_path = None  # <- inicjalizacja ścieżki pliku
 
         self.setWindowTitle("Notepad")
         self.setGeometry(100, 100, 600, 600)
@@ -34,7 +35,48 @@ class MainWindow(QMainWindow):
         file_menu.addSeparator()
         file_menu.addAction(exit_action)
 
+        # podpięcie akcji
+        open_action.triggered.connect(self.open_file)
+        save_action.triggered.connect(self.save_file)
         exit_action.triggered.connect(self.close)
+
+    def open_file(self):
+        path, _ = QFileDialog.getOpenFileName(self, "Otwórz plik", "", "Text Files (*.txt);;All Files (*)")
+
+        if not path:
+            return
+
+        try:
+            with open(path, "r", encoding="utf-8") as f:
+                content = f.read()
+                self.text_edit.setText(content)
+                self.current_path = path
+        except Exception as e:
+            QMessageBox.critical(self, "Błąd", f"Nie można otworzyć pliku:\n{e}")
+
+    def save_file(self):
+        if self.current_path:
+            try:
+                with open(self.current_path, "w", encoding="utf-8") as f:
+                    f.write(self.text_edit.toPlainText())
+                return
+            except Exception as e:
+                QMessageBox.critical(self, "Błąd", f"Nie można zapisać pliku:\n{e}")
+                return
+        self.save_file_as()
+
+    def save_file_as(self):
+        path, _ = QFileDialog.getSaveFileName(self, "Zapisz jako", "", "Text Files (*.txt);;All Files (*)")
+
+        if not path:
+            return
+
+        try:
+            with open(path, "w", encoding="utf-8") as f:
+                f.write(self.text_edit.toPlainText())
+            self.current_path = path
+        except Exception as e:
+            QMessageBox.critical(self, "Błąd", f"Nie można zapisać pliku:\n{e}")
 
 
 def main():
