f602a8a67ac469529fbd416283c9dbc6a0e69727
NyMyTyZy <117938294+NyMyTyZy@users.noreply.github.com>
2025-11-24 14:06:00 +0100

    feat: search bar

--- src/main.py
+++ src/main.py
@@ -1,15 +1,40 @@
 import sys
-from PyQt6.QtWidgets import QMainWindow, QApplication, QTextEdit
+
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTextEdit, QMenuBar
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.menu_bar = None
+        self.text_edit = None
+
         self.setWindowTitle("Notepad")
         self.setGeometry(100, 100, 600, 600)
+        self.setup_editor()
+        self.setup_menu()
 
+    def setup_editor(self):
         self.text_edit = QTextEdit(self)
         self.setCentralWidget(self.text_edit)
+
+    def setup_menu(self):
+        self.menu_bar = QMenuBar(self)
+        self.setMenuBar(self.menu_bar)
+
+        file_menu = self.menu_bar.addMenu("Plik")
+
+        open_action = QAction("Otwórz", self)
+        save_action = QAction("Zapisz", self)
+        exit_action = QAction("Zamknij", self)
+
+        file_menu.addAction(open_action)
+        file_menu.addAction(save_action)
+        file_menu.addSeparator()
+        file_menu.addAction(exit_action)
+
+        exit_action.triggered.connect(self.close)
 
 
 def main():
